e68dcf60c52600a106b93112c0f73ffd3972d28b
Nathan L. Toner <nathan.toner@centauricorp.com>
2020-04-15 11:45:03 -0600

    PEP8 improvements

--- pix2pix.py
+++ pix2pix.py
@@ -16,36 +16,60 @@ from ops import conv, max_pooling, hw_flatten
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--input_dir", help="path to folder containing images")
-parser.add_argument("--mode", required=True, choices=["train", "test", "export"])
-parser.add_argument("--output_dir", required=True, help="where to put output files")
+parser.add_argument("--mode", required=True,
+                    choices=["train", "test", "export"])
+parser.add_argument("--output_dir", required=True,
+                    help="where to put output files")
 parser.add_argument("--seed", type=int)
-parser.add_argument("--checkpoint", default=None, help="directory with checkpoint to resume training from or use for testing")
-
-parser.add_argument("--max_steps", type=int, help="number of training steps (0 to disable)")
-parser.add_argument("--max_epochs", type=int, help="number of training epochs")
-parser.add_argument("--summary_freq", type=int, default=100, help="update summaries every summary_freq steps")
-parser.add_argument("--progress_freq", type=int, default=50, help="display progress every progress_freq steps")
-parser.add_argument("--trace_freq", type=int, default=0, help="trace execution every trace_freq steps")
-parser.add_argument("--display_freq", type=int, default=0, help="write current training images every display_freq steps")
-parser.add_argument("--save_freq", type=int, default=5000, help="save model every save_freq steps, 0 to disable")
-
-parser.add_argument("--separable_conv", action="store_true", help="use separable convolutions in the generator")
-parser.add_argument("--aspect_ratio", type=float, default=1.0, help="aspect ratio of output images (width/height)")
-parser.add_argument("--lab_colorization", action="store_true", help="split input image into brightness (A) and color (B)")
-parser.add_argument("--batch_size", type=int, default=1, help="number of images in batch")
-parser.add_argument("--which_direction", type=str, default="AtoB", choices=["AtoB", "BtoA"])
-parser.add_argument("--ngf", type=int, default=64, help="number of generator filters in first conv layer")
-parser.add_argument("--ndf", type=int, default=64, help="number of discriminator filters in first conv layer")
-parser.add_argument("--scale_size", type=int, default=286, help="scale images to this size before cropping to 256x256")
-parser.add_argument("--flip", dest="flip", action="store_true", help="flip images horizontally")
-parser.add_argument("--no_flip", dest="flip", action="store_false", help="don't flip images horizontally")
-parser.set_defaults(flip=True)
-parser.add_argument("--lr_gen", type=float, default=4e-4, help="initial learning rate for generator adam")
-parser.add_argument("--lr_dsc", type=float, default=1e-4, help="initial learning rate for discriminator adam")
-parser.add_argument("--beta1_gen", type=float, default=0.5, help="momentum term of generator adam")
-parser.add_argument("--beta1_dsc", type=float, default=0.5, help="momentum term of discriminator adam")
-parser.add_argument("--l1_weight", type=float, default=100.0, help="weight on L1 term for generator gradient")
-parser.add_argument("--gan_weight", type=float, default=1.0, help="weight on GAN term for generator gradient")
+parser.add_argument(
+    "--checkpoint",
+    default=None,
+    help="directory with checkpoint to resume training from or use for testing"
+)
+parser.add_argument("--max_steps", type=int,
+                    help="number of training steps (0 to disable)")
+parser.add_argument("--max_epochs", type=int,
+                    help="number of training epochs")
+parser.add_argument("--summary_freq", type=int, default=100,
+                    help="update summaries every summary_freq steps")
+parser.add_argument("--progress_freq", type=int, default=50,
+                    help="display progress every progress_freq steps")
+parser.add_argument("--trace_freq", type=int, default=0,
+                    help="trace execution every trace_freq steps")
+parser.add_argument("--display_freq", type=int, default=0,
+                    help="write current training images every display_freq steps")
+parser.add_argument("--save_freq", type=int, default=5000,
+                    help="save model every save_freq steps, 0 to disable")
+parser.add_argument("--separable_conv", action="store_true",
+                    help="use separable convolutions in the generator")
+parser.add_argument("--aspect_ratio", type=float, default=1.0,
+                    help="aspect ratio of output images (width/height)")
+parser.add_argument("--lab_colorization", action="store_true",
+                    help="split input image into brightness (A) and color (B)")
+parser.add_argument("--batch_size", type=int, default=1,
+                    help="number of images in batch")
+parser.add_argument("--which_direction", type=str, default="AtoB",
+                    choices=["AtoB", "BtoA"])
+parser.add_argument("--ngf", type=int, default=64,
+                    help="number of generator filters in first conv layer")
+parser.add_argument("--ndf", type=int, default=64,
+                    help="number of discriminator filters in first conv layer")
+parser.add_argument("--scale_size", type=int, default=286,
+                    help="scale images to this size before cropping to 256x256")
+parser.add_argument("--flip", dest="flip", action="store_true", default=False,
+                    help="flip images horizontally")
+parser.add_argument("--lr_gen", type=float, default=4e-4,
+                    help="initial learning rate for generator adam")
+parser.add_argument("--lr_dsc", type=float, default=1e-4,
+                    help="initial learning rate for discriminator adam")
+parser.add_argument("--beta1_gen", type=float, default=0.5,
+                    help="momentum term of generator adam")
+parser.add_argument("--beta1_dsc", type=float, default=0.5,
+                    help="momentum term of discriminator adam")
+parser.add_argument("--l1_weight", type=float, default=100.0,
+                    help="weight on L1 term for generator gradient")
+parser.add_argument("--gan_weight", type=float, default=1.0,
+                    help="weight on GAN term for generator gradient")
 parser.add_argument("--n_channels", type=int, default=3,
                     help="Number of channels in image.")
 parser.add_argument("--transform", action="store_true", default=False,
@@ -54,13 +78,20 @@ parser.add_argument("--crop_size", type=int, default=256,
                     help="Size of cropped image chunks.")
 
 # export options
-parser.add_argument("--output_filetype", default="png", choices=["png", "jpeg"])
+parser.add_argument("--output_filetype", default="png",
+                    choices=["png", "jpeg"])
 a = parser.parse_args()
 
 EPS = 1e-12
 
-Examples = collections.namedtuple("Examples", "paths, inputs, targets, count, steps_per_epoch")
-Model = collections.namedtuple("Model", "outputs, predict_real, predict_fake, discrim_loss, discrim_grads_and_vars, gen_loss_GAN, gen_loss_L1, gen_grads_and_vars, train")
+Examples = collections.namedtuple(
+    "Examples",
+    "paths, inputs, targets, count, steps_per_epoch"
+)
+Model = collections.namedtuple(
+    "Model",
+    "outputs, predict_real, predict_fake, discrim_loss, discrim_grads_and_vars, gen_loss_GAN, gen_loss_L1, gen_grads_and_vars, train"
+)
 
 
 def preprocess(image, add_noise=False):
