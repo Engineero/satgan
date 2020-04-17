51e5e75fdfdf724f5297de60cb4d41b5abdeff6d
Engineero <engineerolabs@gmail.com>
2020-04-17 13:36:25 -0600

    Initial attempt at Keras API for discriminator

--- pix2pix.py
+++ pix2pix.py
@@ -510,41 +510,46 @@ def create_task_net(a, input_shape):
 def create_model(a, inputs, targets, task_targets=None):
     input_shape = inputs.shape.as_list()
     def create_discriminator(a, discrim_inputs, discrim_targets):
+        """Creates the discriminator network.
+
+        Args:
+            a: command-line arguments object.
+            discrim_inputs: input images (generator priors).
+            discrim_targets: target_images, real or generator outputs.
+
+        Returns:
+            Discriminator network model.
+        """
         n_layers = 3
-        layers = []
 
         # 2x [batch, height, width, in_channels] => [batch, height, width, in_channels * 2]
-        input_concat = tf.concat([discrim_inputs, discrim_targets], axis=3)
+        x_in = Input(shape=discrim_inputs.shape.as_list())
+        y_in = Input(shape=discrim_targets.shape.as_list())
+        input_concat = Concatenate([x_in, y_in], axis=3)
 
         # layer_1: [batch, 256, 256, in_channels * 2] => [batch, 128, 128, ndf]
-        with tf.variable_scope("layer_1"):
-            convolved = discrim_conv(input_concat, a.ndf, stride=2)
-            rectified = lrelu(convolved, 0.2)
-            layers.append(rectified)
+        x = ops.down_resblock(input_concat, filters=a.ndf,
+                              sn=a.spec_norm, scope='layer_1')
 
         # layer_2: [batch, 128, 128, ndf] => [batch, 64, 64, ndf * 2]
         # layer_3: [batch, 64, 64, ndf * 2] => [batch, 32, 32, ndf * 4]
         # layer_4: [batch, 32, 32, ndf * 4] => [batch, 31, 31, ndf * 8]
         for i in range(n_layers):
-            with tf.variable_scope("layer_%d" % (len(layers) + 1)):
-                out_channels = a.ndf * min(2**(i+1), 8)
-                stride = 1 if i == n_layers - 1 else 2  # last layer here has stride 1
-                convolved = discrim_conv(layers[-1], out_channels, stride=stride)
-                normalized = batchnorm(convolved)
-                rectified = lrelu(normalized, 0.2)
-                layers.append(rectified)
+            out_channels = a.ndf * min(2**(i+1), 8)
+            x = ops.down_resblock(x, filters=out_channels, sn=a.spec_norm,
+                                  scope=f'layer_{i+1}')
+            x = BatchNormalization()(x)
 
         # layer_5: [batch, 31, 31, ndf * 8] => [batch, 30, 30, 1]
-        with tf.variable_scope("layer_%d" % (len(layers) + 1)):
-            convolved = discrim_conv(rectified, out_channels=1, stride=1)
-            output = tf.sigmoid(convolved)
-            layers.append(output)
+        x = ops.down_resblock(x, filters=1, to_down=False, sn=a.spec_norm,
+                              scope=f'layer_{n_layers + 1}')
+        x = tf.nn.sigmoid(x)
 
-        return layers[-1]
+        return Model(inputs=[x_in, y_in], outputs=x)
 
-    with tf.variable_scope("generator"):
+    with tf.name_scope("generator"):
         out_channels = int(targets.get_shape()[-1])
-        outputs = create_generator(a, input_shape, out_channels)
+        generator = create_generator(a, input_shape, out_channels)
 
     # Create two copies of the task network, one for real images (targets
     # input to this method) and one for generated images (outputs from
@@ -555,15 +560,17 @@ def create_model(a, inputs, targets, task_targets=None):
 
     # Create two copies of discriminator, one for real pairs and one for fake
     # pairs they share the same underlying variables
-    with tf.name_scope("real_discriminator"):
-        with tf.variable_scope("discriminator"):
+    with tf.name_scope("discriminator"):
+        # TODO (NLT): figure out discriminator loss, interaction with Keras changes.
+        # discriminator = create_discriminator(a, inputs, targets)
+        with tf.compat.v1.variable_scope("discriminator"):
             # 2x [batch, height, width, channels] => [batch, 30, 30, 1]
-            predict_real = create_discriminator(a, input_shape, targets)
+            predict_real = create_discriminator(a, inputs, targets)
 
     with tf.name_scope("fake_discriminator"):
-        with tf.variable_scope("discriminator", reuse=True):
+        with tf.compat.v1.variable_scope("discriminator", reuse=True):
             # 2x [batch, height, width, channels] => [batch, 30, 30, 1]
-            predict_fake = create_discriminator(a, input_shape, outputs)
+            predict_fake = create_discriminator(a, inputs, generator.outputs[0])
 
     with tf.name_scope("discriminator_loss"):
         # minimizing -tf.log will try to get inputs to 1
