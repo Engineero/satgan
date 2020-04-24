db4490430c0418324cbc55b02aa117f16cf87a5f
Engineero <engineerolabs@gmail.com>
2020-04-24 10:17:21 -0600

    Added check for list input to _bytes_feature

--- utils/make_tfrecords.py
+++ utils/make_tfrecords.py
@@ -76,20 +76,23 @@ def _group_list(ungrouped_list, group_size, padding=None):
 # The following functions can be used to convert a value to a type compatible
 # with tf.Example.
 def _bytes_feature(value):
-  """Returns a bytes_list from a string / byte."""
-  if isinstance(value, type(tf.constant(0))):
-    value = value.numpy() # BytesList won't unpack a string from an EagerTensor.
-  return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+    """Returns a bytes_list from a string / byte."""
+    if isinstance(value, type(tf.constant(0))):
+      value = value.numpy() # BytesList won't unpack a string from an EagerTensor.
+    if isinstance(value, list):
+        return tf.train.Feature(bytes_list=tf.train.BytesList(value=value))
+    else:
+        return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 
 
 def _float_feature(value):
-  """Returns a float_list from a float / double."""
-  return tf.train.Feature(float_list=tf.train.FloatList(value=[value]))
+    """Returns a float_list from a float / double."""
+    return tf.train.Feature(float_list=tf.train.FloatList(value=[value]))
 
 
 def _int64_feature(value):
-  """Returns an int64_list from a bool / enum / int / uint."""
-  return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+    """Returns an int64_list from a bool / enum / int / uint."""
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
 
 
 def _read_fits(path):
