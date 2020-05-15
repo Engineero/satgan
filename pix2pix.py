3acb630c4d7b96466ec934a23cebf88cd0efd48c
Engineero <engineerolabs@gmail.com>
2020-05-15 12:45:28 -0600

    Rolling back last change. Didn't work.

--- pix2pix.py
+++ pix2pix.py
@@ -564,8 +564,7 @@ def create_model(a, train_data):
                   'tf_op_layer_task_net_2': opt_task}
     model.compile(loss=losses,
                   loss_weights=loss_weights,
-                  optimizer=Adam(),
-                  run_eagerly=True)
+                  optimizer=Adam())
     return model
 
 
