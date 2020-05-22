43653786009fa53a0397b47bf71cd9f60f9bb8c5
Engineero <engineerolabs@gmail.com>
2020-05-22 13:38:09 -0600

    Fixed bug in legacy code that got missed in some refactors

--- pix2pix.py
+++ pix2pix.py
@@ -496,9 +496,8 @@ def main(a):
                         loss = weight * loss_function(model_inputs,
                                                       model_outputs,
                                                       step)
-                    watched = tape.watched_variables()
-                    gradients = tape.gradient(loss, watched)
-                    optimizer.apply_gradients(zip(gradients, watched))
+                    gradients = tape.gradient(loss, model.trainable_variables)
+                    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
             else:
                 for optimizer, loss_function in zip(optimizer_list,
                                                     loss_function_list):
