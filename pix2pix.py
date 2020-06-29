b8e817f8f1223a4aa5289d10bc2fd312627aecf6
Engineero <engineerolabs@gmail.com>
2020-06-29 13:26:45 -0600

    Revert "Updated parsing method for task net to match bbox stacking axis of yolo"
    This reverts commit a42411bfe729bf72051ba07fe33fc18e69e78626.

--- pix2pix.py
+++ pix2pix.py
@@ -50,7 +50,7 @@ def preprocess(image, add_noise=False):
 def _parse_example(serialized_example, a):
     """Parses a single TFRecord Example for the task network."""
     # Parse serialized example.
-    example = tf.io.parse_single_example(
+    example = tf.io.parse_example(
         serialized_example,
         {
             'a_raw': tf.io.VarLenFeature(dtype=tf.string),
@@ -91,19 +91,14 @@ def _parse_example(serialized_example, a):
     b_image = tf.reshape(b_image, [-1, height[0], width[0], 1])
 
     # Package things up for output.
-    objects = tf.stack([ymin, xmin, ymax, xmax, classes], axis=1)
-    print(f'objects shape: {objects.shape}')
+    objects = tf.stack([ymin, xmin, ymax, xmax, classes], axis=-1)
 
     # Need to pad objects to max inferences (not all images will have same
     # number of objects).
-    paddings = tf.constant([[0, a.max_inferences], [0, 0]])
-    print(f'paddings 1 shape: {paddings.shape}')
-    paddings = paddings - (tf.constant([[0, 1], [0, 0]]) * tf.shape(objects)[1])
-    print(f'paddings 2 shape: {paddings.shape}')
+    paddings = tf.constant([[0, 0], [0, a.max_inferences], [0, 0]])
+    paddings = paddings - (tf.constant([[0, 0], [0, 1], [0, 0]]) * tf.shape(objects)[1])
     objects = tf.pad(tensor=objects, paddings=paddings, constant_values=0.)
-    print(f'padded objects shape: {objects.shape}')
-    objects = tf.tile(objects, [a.num_pred_layers, 1])
-    print(f'tiled objects shape: {objects.shape}')
+    objects = tf.tile(objects, [1, a.num_pred_layers, 1])
 
     # task_targets = (objects, width, height)
     if a.which_direction == 'AtoB':
