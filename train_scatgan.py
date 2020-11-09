9b6df079f71275da437c7db9ea81cfa6ddd43cf7
Nathan L. Toner <nathan.toner@centauricorp.com>
2020-11-09 12:03:56 -1000

    Added (commented out) debug prints

--- train_scatgan.py
+++ train_scatgan.py
@@ -125,6 +125,10 @@ def main(a):
                     model_outputs = (gen_outputs, discrim_outputs,
                                      task_outputs)
 
+                    # print(f'\nA targets: {a_task_targets}')
+                    # print(f'\nB targets: {b_task_targets}')
+                    # print(f'\ntask net outputs: {task_outputs}')
+
                     # Plot all of the summary images.
                     plot_summaries(a, model_inputs, model_outputs,
                                    batches_seen)
