Updated image normalization to match what it should be for TF1 and (corrected) TF2 input pipeline for YOLO

model/data_loader.py
@@ -40,9 +40,8 @@ class GanDataset:
         """
 
         with tf.name_scope("preprocess"):
+            image = tf.cast(image, tf.float32)
             image = tf.image.per_image_standardization(image)
-            image = tf.image.convert_image_dtype(image, tf.float32)
-            # image = tf.cast(image, tf.float32)
             return image
 
     def _create_dataset(self):
@@ -153,9 +152,7 @@ def _convert_batches(batch):
     """
 
     image, bboxes, _ = batch
-    # image = tf.cast(image, tf.float32)
-    # image = tf.image.convert_image_dtype(image, tf.float32)
-    return image, bboxes
+    return tf.cast(image, tf.float32), bboxes
 
 
 def load_examples(a, data_dir, shuffle=False, pad_bboxes=False, encoder=None):

model/losses.py
@@ -4,7 +4,6 @@
 import tensorflow as tf
 from tensorflow.keras.losses import (MSE, mean_absolute_error,
                                      categorical_crossentropy)
-from .utils.plot_summaries import image_int_to_float
 
 
 # Define model losses and helpers for computing and applying gradients.
@@ -200,7 +199,7 @@ def calc_generator_loss(a, model_inputs, model_outputs, step,
                                       [discrim_fake.shape[0], -1, 2])
             targets_ones = tf.ones(shape=discrim_fake.shape[:-1])
             targets_zeros = tf.zeros(shape=discrim_fake.shape[:-1])
-            targets = image_int_to_float(model_inputs[1])
+            targets = model_inputs[1]
             gen_loss_GAN = tf.reduce_mean(
                 categorical_crossentropy(
                     tf.stack([targets_zeros, targets_ones], axis=-1),

model/scatgan.py
@@ -4,7 +4,6 @@
 from .generator import create_generator
 from .discriminator import create_discriminator
 from .task_net import create_task_net
-from .utils.plot_summaries import image_int_to_float, image_float_to_int
 
 from pathlib import Path
 import tensorflow as tf
@@ -54,7 +53,7 @@ def create_model(a, a_train_data, b_train_data):
                 generator = create_generator(a, input_shape, out_channels)
                 generator.summary()
                 gen_noise = generator(noise)
-                fake_img = gen_noise + image_int_to_float(inputs)
+                fake_img = gen_noise + inputs
                 gen_outputs = tf.stack([fake_img, gen_noise], axis=0,
                                        name='generator')
 
@@ -64,7 +63,7 @@ def create_model(a, a_train_data, b_train_data):
                 # TODO (NLT): figure out discriminator loss, interaction with Keras changes.
                 discriminator = create_discriminator(a, target_shape)
                 discriminator.summary()
-                predict_real = discriminator(image_int_to_float(targets))  # should -> [0, 1]
+                predict_real = discriminator(targets)  # should -> [0, 1]
                 predict_fake = discriminator(fake_img)  # should -> [1, 0]
                 discrim_outputs = tf.stack([predict_real, predict_fake], axis=0,
                                            name='discriminator')
@@ -94,8 +93,8 @@ def create_model(a, a_train_data, b_train_data):
                 else:
                     task_net = create_task_net(a, input_shape)
                 pred_task = task_net(targets)
-                pred_task_fake = task_net(image_float_to_int(fake_img))
-                pred_task_noise = task_net(image_float_to_int(gen_noise))
+                pred_task_fake = task_net(fake_img)
+                pred_task_noise = task_net(gen_noise)
             task_net.summary()
             task_outputs = tf.stack([pred_task, pred_task_fake, pred_task_noise],
                                     axis=0,

model/utils/plot_summaries.py
@@ -11,12 +11,6 @@ def image_int_to_float(image):
     return tf.image.convert_image_dtype(image, tf.float32)
 
 
-def image_float_to_int(image):
-    """Converts a float32 image in range [0, 1] to a float image in uint16 range."""
-    image = tf.image.convert_image_dtype(image, tf.uint16)
-    return tf.cast(image, tf.float32)
-
-
 def plot_summaries(a, model_inputs, model_outputs, batches_seen):
     """Plots summary images to Tensorboard.
 
@@ -119,7 +113,7 @@ def plot_summaries(a, model_inputs, model_outputs, batches_seen):
         colors=np.array([[0., 1., 0.]])
     )
     generated_bboxes = tf.image.draw_bounding_boxes(
-        images=tf.image.grayscale_to_rgb(gen_outputs[0]),
+        images=tf.image.grayscale_to_rgb(image_int_to_float(gen_outputs[0])),
         boxes=a_true_bboxes,
         colors=np.array([[1., 0., 0.]])
     )
@@ -129,7 +123,7 @@ def plot_summaries(a, model_inputs, model_outputs, batches_seen):
         colors=np.array([[0., 1., 0.]])
     )
     noise_bboxes = tf.image.draw_bounding_boxes(
-        images=tf.image.grayscale_to_rgb(gen_outputs[1]),
+        images=tf.image.grayscale_to_rgb(image_int_to_float(gen_outputs[1])),
         boxes=n_fake_bboxes,
         colors=np.array([[0., 1., 0.]])
     )