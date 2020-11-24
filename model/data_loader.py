2a5ec70ccd0e21605605609d055419680bc37a03
Engineero <engineerolabs@gmail.com>
2020-11-24 11:22:09 -0700

    Added multiframe data parsing to data_loader

--- model/data_loader.py
+++ model/data_loader.py
@@ -58,10 +58,16 @@ class GanDataset:
         # Specify transformations on datasets.
         if self.shuffle:
             data = data.shuffle(self.a.buffer_size)
-        data = data.map(
-            lambda x: self._parse_example(x),
-            num_parallel_calls=self.a.num_parallel_calls
-        )
+        if self.a.is_recurrent:
+            data = data.map(
+                lambda x: self._parse_example_multiframe(x),
+                num_parallel_calls=self.a.num_parallel_calls
+            )
+        else:
+            data = data.map(
+                lambda x: self._parse_example(x),
+                num_parallel_calls=self.a.num_parallel_calls
+            )
         data = data.batch(self.a.batch_size, drop_remainder=True)
         data = data.prefetch(buffer_size=self.a.buffer_size)
         return data
@@ -139,6 +145,143 @@ class GanDataset:
         # Normalize and convert the image, then return (inputs, targets) tuple.
         image = self._preprocess(image)
         return image, objects
+
+    def _parse_example_multiframe(self, serialized_example):
+        """Parses a single TFRecord Example for one domain of the task network.
+
+        Args:
+            serialized_example: TFRecord example to load/interpret.
+            a: argparse object from training script.
+
+        Keyword Args:
+            pad_bboxes: if True, pads truth bboxes with +/-10 pix.
+
+        Returns:
+            Images, optional noise or None, and true bounding boxes with
+            classifications (objects) as:
+                (image, noise), objects
+        """
+
+        # Parse serialized example.
+        example = tf.io.parse_single_example(
+            serialized_example,
+            {
+                'images_raw': tf.io.VarLenFeature(dtype=tf.string),
+                'filename': tf.io.VarLenFeature(dtype=tf.string),
+                'height': tf.io.FixedLenFeature(shape=(), dtype=tf.int64),
+                'width': tf.io.FixedLenFeature(shape=(), dtype=tf.int64),
+                'num_time_steps': tf.io.FixedLenFeature([], dtype=tf.int64),
+                'classes': tf.io.VarLenFeature(dtype=tf.int64),
+                'ymin': tf.io.VarLenFeature(dtype=tf.float32),
+                'ymax': tf.io.VarLenFeature(dtype=tf.float32),
+                'ycenter': tf.io.VarLenFeature(dtype=tf.float32),
+                'xmin': tf.io.VarLenFeature(dtype=tf.float32),
+                'xmax': tf.io.VarLenFeature(dtype=tf.float32),
+                'xcenter': tf.io.VarLenFeature(dtype=tf.float32),
+                'magnitude': tf.io.VarLenFeature(dtype=tf.float32),
+                'classes_shape': tf.io.VarLenFeature(tf.int64),
+                'ymin_shape': tf.io.VarLenFeature(tf.int64),
+                'ymax_shape': tf.io.VarLenFeature(tf.int64),
+                'xmin_shape': tf.io.VarLenFeature(tf.int64),
+                'xmax_shape': tf.io.VarLenFeature(tf.int64),
+                'xcenter_shape': tf.io.VarLenFeature(tf.int64),
+                'ycenter_shape': tf.io.VarLenFeature(tf.int64),
+            }
+        )
+
+        # Cast parsed objects into usable types.
+        width = tf.cast(example['width'], tf.int32)
+        height = tf.cast(example['height'], tf.int32)
+        num_time_steps = tf.cast(example["num_time_steps"], tf.int32)
+        classes = _parse_array(
+            example["classes"],
+            example["classes_shape"],
+            saved_dtype=tf.float64,
+            name="classes",
+        )
+
+        if self.pad_bboxes:
+            # Pad bounding boxes. SatSim makes really tight bboxes...
+            xcenter = _parse_array(
+                example["xcenter"],
+                example["xcenter_shape"],
+                saved_dtype=tf.float64,
+                name="xcenter",
+            )
+            ycenter = _parse_array(
+                example["ycenter"],
+                example["ycenter_shape"],
+                saved_dtype=tf.float64,
+                name="ycenter",
+            )
+            xmin = xcenter - 10. / tf.cast(width, tf.float32)
+            xmax = xcenter + 10. / tf.cast(width, tf.float32)
+            ymin = ycenter - 10. / tf.cast(height, tf.float32)
+            ymax = ycenter + 10. / tf.cast(height, tf.float32)
+        else:
+            # Grab bboxes directly from data.
+            ymin = _parse_array(
+                example["ymin"],
+                example["ymin_shape"],
+                saved_dtype=tf.float64,
+                name="ymin",
+            )
+            ymax = _parse_array(
+                example["ymax"],
+                example["ymax_shape"],
+                saved_dtype=tf.float64,
+                name="ymax",
+            )
+            xmin = _parse_array(
+                example["xmin"],
+                example["xmin_shape"],
+                saved_dtype=tf.float64,
+                name="xmin",
+            )
+            xmax = _parse_array(
+                example["xmax"],
+                example["xmax_shape"],
+                saved_dtype=tf.float64,
+                name="xmax",
+            )
+
+        # Parse images and preprocess.
+        image = tf.sparse.to_dense(example['images_raw'], default_value='')
+        image = tf.io.decode_raw(image, tf.uint16)
+        image = tf.reshape(
+            image,
+            [num_time_steps, height, width, self.a.n_channels]
+        )
+
+        # Package things up for output.
+        objects = tf.stack([ymin, xmin, ymax, xmax, classes], axis=-1)
+
+        # Need to pad objects to max inferences (not all images will have same
+        # number of objects).
+        paddings = tf.constant([[0, 0], [0, self.a.max_inferences], [0, 0]])
+        paddings = paddings - (
+            tf.constant([[0, 0], [0, 1], [0, 0]]) * tf.shape(objects)[0]
+        )
+        objects = tf.pad(tensor=objects, paddings=paddings, constant_values=0.)
+        objects = tf.tile(objects, [self.a.num_pred_layers, 1])
+
+        # Normalize and convert the image, then return (inputs, targets) tuple.
+        image = self._preprocess(image)
+        return image, objects
+
+
+def _parse_array(sparse_tensor, sparse_shape, saved_dtype, name="parse_array"):
+    # We need to pull the raw tensors from the TFRecords
+    tensor_shape = tf.cast(tf.sparse.to_dense(sparse_shape), tf.int32)
+    tensor = tf.sparse.to_dense(sparse_tensor, default_value="")
+
+    # Raw decode and reshape the array
+    tensor = tf.io.decode_raw(tensor, saved_dtype, name=name + "_decode_raw")
+    tensor = tf.reshape(tensor, tensor_shape, name=name + "_reshape")
+
+    # Cast everything to float32 when we are done
+    tensor = tf.cast(tensor, tf.float32, name=name + "_cast")
+    return tensor
 
 
 def _convert_batches(batch):
